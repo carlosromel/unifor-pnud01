b5fd42c40da193f5ac52d13a4ae75de18fde7464
Carlos Romel Pereira da Silva <carlos.pereira@trt18.jus.br>
2023-10-18 12:09:58 -0300

    Incluir primeiro exercício

---
+++ HelloWorld/src/br/unifor/pnud01/HelloWorld.java
@@ -0,0 +1,8 @@
+package br.unifor.pnud01;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.printf("Olá mundo!\n");
+	}
+}
---
+++ HelloWorld/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author s165484
+ *
+ */
+module HelloWorld {
+}
